8c323b956de3463afcc7e94bbb5bbba693084f94
Claire Li <claireli100@gmail.com>
2020-04-26 21:55:07 -0700

    walking through directory

--- preprocess.py
+++ preprocess.py
@@ -1,16 +1,24 @@
-# TODO: write loop here to go to every subdir, and every text file
-directory_name="malicia/zeroaccess/fd6b61857f59334e95aa12c9336425834cd404e8.asm.txt"
+import os
 
-f=open(directory_name, "r")
-lines=f.readlines()
+for i in os.walk('malicia/'):
+    print(i[0])
+    for k in os.walk(i[0]):
+      print(k[2])
+#directory_name="malicia/zeroaccess/fd6b61857f59334e95aa12c9336425834cd404e8.asm.txt"
 
-op_codes={}
+#parent_dir="malicia/"
 
-for l in lines:
-  if l.strip() not in op_codes.keys():
-    op_codes[l.strip()]=1
-  else:
-    op_codes[l.strip()]+=1
 
-print(op_codes)
+#f=open(directory_name, "r")
+#lines=f.readlines()
+
+#op_codes={}
+
+#for l in lines:
+  #if l.strip() not in op_codes.keys():
+    #op_codes[l.strip()]=1
+  #else:
+    #op_codes[l.strip()]+=1
+
+#print(op_codes)
 
